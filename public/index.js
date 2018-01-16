117c1676b3900e6ecd4ac569e26a3ae734d9e5a3
Kiril Vatev <vatev.1@gmail.com>
2018-01-15 21:04:57 -0500

    sigh, you can't get all posts, but starting to work on rendering anyway, just for fun

--- public/index.js
+++ public/index.js
@@ -1,4 +1,4 @@
-/* jshint browser: true */
+/* jshint browser: true, devel: true */
 /* globals request, Promise, TOKEN */
 
 window.addEventListener('load', function () {
@@ -24,6 +24,10 @@ window.addEventListener('load', function () {
     );
   }
 
+  function date(createdTime) {
+    return new Date(createdTime * 1000);
+  }
+
   var api = {
     media: function (sinceId) {
       return new Promise(function (resolve, reject) {
@@ -33,21 +37,101 @@ window.addEventListener('load', function () {
             token: TOKEN
           }
         );
-        request.jsonp(url, function (err, resp, body) {
+
+        if (sinceId) {
+          url += '&max_id=' + sinceId;
+        }
+
+        request.jsonp({
+          url: url,
+          unknownErrors: true
+        }, function (err, body) {
           if (err) {
             return reject(err);
           }
 
+          if (body.meta && body.meta.code !== 200) {
+            return reject(new Error('unknown meta code ' + body.meta.code));
+          }
+
           return resolve(body);
+        });
+      });
+    },
+    photos: function (sinceId) {
+      return api.media(sinceId).then(function (body) {
+        if (body.data.length === 0) {
+          return [];
+        }
+
+        return body.data.filter(function (post) {
+          return post.type === 'image';
         });
       });
     }
   };
 
   var loginButton = document.querySelector('#login');
+  var getImagesButton = document.querySelector('#get-images');
+  var imagesDiv = document.querySelector('#images');
 
   loginButton.onclick = function () {
     loginRedirect();
+  };
+
+  function image(url) {
+    var img = document.createElement('img');
+    img.src = url;
+
+    return img;
+  }
+
+  function svg(urls) {
+    return renderMustache(
+      '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 99 99">' +
+        '<image width="33" height="33" x="0"  y="0"  xlink:href="${0}" preserveAspectRatio="xMidYMid slice" />' +
+        '<image width="33" height="33" x="33" y="0"  xlink:href="${1}" preserveAspectRatio="xMidYMid slice" />' +
+        '<image width="33" height="33" x="66" y="0"  xlink:href="${2}" preserveAspectRatio="xMidYMid slice" />' +
+        '<image width="33" height="33" x="0"  y="33" xlink:href="${3}" preserveAspectRatio="xMidYMid slice" />' +
+        '<image width="33" height="33" x="33" y="33" xlink:href="${4}" preserveAspectRatio="xMidYMid slice" />' +
+        '<image width="33" height="33" x="66" y="33" xlink:href="${5}" preserveAspectRatio="xMidYMid slice" />' +
+        '<image width="33" height="33" x="0"  y="66" xlink:href="${6}" preserveAspectRatio="xMidYMid slice" />' +
+        '<image width="33" height="33" x="33" y="66" xlink:href="${7}" preserveAspectRatio="xMidYMid slice" />' +
+        '<image width="33" height="33" x="66" y="66" xlink:href="${8}" preserveAspectRatio="xMidYMid slice" />' +
+      '</svg>', urls);
+  }
+
+  getImagesButton.onclick = function () {
+    var count = 0;
+
+    api.photos()
+    .then(function handleBody(posts) {
+      if (!posts.length) {
+        return;
+      }
+
+      count += posts.length;
+
+      var nine = posts.slice(0, 9).map(function (post) {
+        return post.images.standard_resolution.url;
+      });
+
+      var content = svg(nine);
+
+      imagesDiv.innerHTML = content;
+
+//      posts.forEach(function (post) {
+//        imagesDiv.appendChild(image(post.images.standard_resolution.url));
+//      });
+
+//      if (count < 25) {
+//        var lastId = posts[posts.length - 1].id;
+//
+//        return api.photos(lastId).then(handleBody);
+//      }
+    }).catch(function (err) {
+      console.log(err);
+    });
   };
 
 });
