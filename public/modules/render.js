fbfc01fed89c0d4528a79cbcffc20f5e269f8e7b
Kiril Vatev <vatev.1@gmail.com>
2018-02-15 21:28:04 -0500

    moving promises around

--- public/modules/render.js
+++ public/modules/render.js
@@ -167,17 +167,17 @@
           return Promise.resolve(canvas);
         }).then(function (canvas) {
           return renderStatsToCanvas(allPosts, canvas);
-        }).then(function (canvas) {
+        });
+      }).then(function (canvas) {
 
+        dom.empty(imagesDiv);
+        dom.append(imagesDiv, canvas);
+
+        // get the data from the canvas and render it as an
+        // image element
+        return getLoadedImage(canvas.toDataURL('image/png')).then(function (img) {
           dom.empty(imagesDiv);
-          dom.append(imagesDiv, canvas);
-
-          // get the data from the canvas and render it as an
-          // image element
-          return getLoadedImage(canvas.toDataURL('image/png')).then(function (img) {
-            dom.empty(imagesDiv);
-            dom.append(imagesDiv, img);
-          });
+          dom.append(imagesDiv, img);
         });
       });
     }
